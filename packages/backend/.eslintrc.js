c6a3dcdf36aa8555b0cdf8da37b65876b1f6e73f
AmitMizrahii <miamit112@gmail.com>
2026-03-18 23:22:41 +0200

    initiate

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module', project: './tsconfig.json' },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
