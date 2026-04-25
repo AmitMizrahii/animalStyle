83acdf51861d6b78eda0aa5b0ab8c143f71545d7
AmitMizrahii <miamit112@gmail.com>
2026-04-25 17:48:33 +0300

    added https support

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,13 +1,28 @@
+import fs from "fs";
+import http from "http";
+import https from "https";
 import initializeApp from "./index";
 import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 3001;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
 initializeApp()
   .then((app) => {
-    app.listen(PORT, () => {
-      logger.info(`Server running on http://localhost:${PORT}`);
-    });
+    if (process.env.NODE_ENV !== "production") {
+      console.log("DEVELOPMENT MODE: Starting HTTP server");
+      http.createServer(app).listen(PORT, () => {
+        logger.info(`HTTP Server running on http://localhost:${PORT}`);
+      });
+
+      const sslOptions = {
+        key: fs.readFileSync("../client-key.pem"),
+        cert: fs.readFileSync("../client-cert.pem"),
+      };
+      https.createServer(sslOptions, app).listen(HTTPS_PORT, () => {
+        logger.info(`Server running on http://localhost:${HTTPS_PORT}`);
+      });
+    }
   })
   .catch((error: Error) => {
     logger.error("Failed to start server", {
